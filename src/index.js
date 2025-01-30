3ea141c2fa5cc195e235ddfa2e7943360d3e82ae
Byaombe <34215129+byaombe@users.noreply.github.com>
2025-01-30 12:33:20 -0600

    configured to handle html, css, image loader, server, and used merge() to configure prod and dev files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+//import { greeting } from "./greeting.js";
+
+/* import and use an image
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+*/
